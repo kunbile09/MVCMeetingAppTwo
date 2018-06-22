9e1a581aa66eab6e346108b2f7b2adf156600a75
Ali Mohamed <Ali.Mohamed@icstars-columbus.org>
2018-06-21 23:15:10 -0400

    Project Updated

---
+++ WPGMeetingPortal/WPGMeetingPortal/Controllers/SchedulesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WPGMeetingPortal.Controllers
+{
+    [Authorize(Roles = "E")]
+    public class SchedulesController : Controller
+    {
+        // GET: Schedules
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WPGMeetingPortal/WPGMeetingPortal/Models/EmployeeMeetings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WPGMeetingPortal.DAL;
+
+namespace WPGMeetingPortal.Models
+{
+    public class EmployeeMeetings
+    {
+        public int EmployeeId { get; set; }
+        public List<Meeting> Meetings { get; set; }
+    }
+}
